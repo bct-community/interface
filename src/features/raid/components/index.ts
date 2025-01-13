da39a545e1e5670d96b8a18cd4a5b5d016eca662
BCT Community <bctcommunity01@gmail.com>
2025-01-13 10:17:06 -0300

    feat: remove unused Heading and PrettyDate exports from raid components

--- src/features/raid/components/index.ts
+++ src/features/raid/components/index.ts
@@ -1,6 +1,4 @@
-export { default as Heading } from "./Heading";
 export { default as Player } from "./Player";
-export { default as PrettyDate } from "./PrettyDate";
 export { default as RaidTarget } from "./RaidTarget";
 export { default as RaidTargetLogo } from "./RaidTargetLogo";
 export { default as Header } from "./Header";
