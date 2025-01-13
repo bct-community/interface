3b263e151debbadb039995ee114610d3d3d406d0
BCT Community <bctcommunity01@gmail.com>
2025-01-13 12:40:51 -0300

    refactor: simplify Footer component layout and remove year display

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,8 @@
 const Footer = () => {
-  const year = new Date().getFullYear();
-
   return (
-    <footer className="mt-6 flex h-[75px] w-full select-none items-center justify-between px-6 py-2 text-center">
-      <div className="w-[40%] rounded-[12px] border bg-transparent bg-opacity-20 p-4 backdrop-blur-md backdrop-filter">
+    <footer className="my-6 flex h-[75px] w-full select-none items-center justify-between px-4 py-2 text-center">
+      <div className="w-full rounded-[12px] border bg-transparent bg-opacity-20 p-4 backdrop-blur-md backdrop-filter">
         Feito com ❤ pela Comunidade $BCT
-      </div>
-      <div className="w-[40%] rounded-[12px] border bg-transparent bg-opacity-20 p-4 backdrop-blur-md backdrop-filter">
-        © {year} All rights reserved.
       </div>
     </footer>
   );
