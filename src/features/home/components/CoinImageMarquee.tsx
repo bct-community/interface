83850dbbdd4900a9bf189f776cfb0de5dc0ce74c
BCT Community <bctcommunity01@gmail.com>
2025-01-20 12:34:36 -0300

    refactor: wrap Marquee component in a div for improved structure in CoinImageMarquee

--- src/features/home/components/CoinImageMarquee.tsx
+++ src/features/home/components/CoinImageMarquee.tsx
@@ -7,23 +7,21 @@ const CoinImageMarquee = ({ upSideDown }: { upSideDown?: boolean }) => {
   const direction = upSideDown ? "left" : "right";
 
   return (
-    <Marquee
-      className={classNames({
-        "relative z-0 flex h-[110px] w-full select-none items-center overflow-hidden":
-          true,
-        "rotate-180": upSideDown,
-      })}
-      direction={direction}
-      autoFill
-    >
-      <img
-        src={tokenImg}
-        className="mx-7 w-[100px] translate-y-[50%] transform overflow-hidden transition-transform duration-300 hover:translate-y-[5%]"
-        draggable={false}
-      />
+    <div className={classNames({ "w-full": true, "rotate-180": upSideDown })}>
+      <Marquee
+        className="relative z-0 flex h-[110px] w-full select-none items-center overflow-hidden"
+        direction={direction}
+        autoFill
+      >
+        <img
+          src={tokenImg}
+          className="mx-7 w-[100px] translate-y-[50%] transform overflow-hidden transition-transform duration-300 hover:translate-y-[5%]"
+          draggable={false}
+        />
 
-      <div className="z-1 absolute bottom-0 left-0 h-[3px] w-full translate-y-[200%] bg-[var(--coin-pink)] dark:bg-black"></div>
-    </Marquee>
+        <div className="z-1 absolute bottom-0 left-0 h-[3px] w-full translate-y-[200%] bg-[var(--coin-pink)] dark:bg-black"></div>
+      </Marquee>
+    </div>
   );
 };
 
