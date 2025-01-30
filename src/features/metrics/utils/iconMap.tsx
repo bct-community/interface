2a348e50b09a5faba8512e8233b39b2e97282950
BCT Community <bctcommunity01@gmail.com>
2025-01-29 23:52:06 -0300

    refactor: extract icon and logo mappings into separate utility files

---
+++ src/features/metrics/utils/iconMap.tsx
@@ -0,0 +1,15 @@
+import {
+  SiDiscord,
+  SiGithub,
+  SiSolana,
+  SiTelegram,
+  SiX,
+} from "@icons-pack/react-simple-icons";
+
+export const iconMap: Record<string, JSX.Element> = {
+  SiGithub: <SiGithub size={32} />,
+  SiDiscord: <SiDiscord size={32} />,
+  SiX: <SiX size={32} />,
+  SiTelegram: <SiTelegram size={32} />,
+  SiSolana: <SiSolana size={32} />,
+};
