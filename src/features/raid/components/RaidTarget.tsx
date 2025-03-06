efd99e97c81501283a261dd30b03a03b90391409
BCT Community <bctcommunity01@gmail.com>
2025-03-05 23:43:34 -0300

    feat: implement WebSocket integration for real-time raid messages and loading state management

--- src/features/raid/components/RaidTarget.tsx
+++ src/features/raid/components/RaidTarget.tsx
@@ -5,6 +5,7 @@ import { useInView } from "react-intersection-observer";
 import ReactMarkdown from "react-markdown";
 import { TwitterShareButton, XIcon } from "react-share";
 import remarkGfm from "remark-gfm";
+import { type Socket, io } from "socket.io-client";
 
 import { Alert } from "@/components";
 import { Button } from "@/components/ui/button";
@@ -19,25 +20,34 @@ import { Progress } from "@/components/ui/progress";
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
   SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { Skeleton } from "@/components/ui/skeleton";
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import env from "@/config";
+import { BlockedMessagePayload, BotMessage } from "@/features/chat/types";
 
 import { useRaidData } from "../api/getRaidData";
 import { useRegisterChatMessageInRaid } from "../api/registerChatMessageInRaid";
 import { useRegisterRaidAccess } from "../api/registerRaidAccess";
 import RaidSkeleton from "./RaidSkeleton";
 
+const socket: Socket = io(env.VITE_WS_URL, {
+  transports: ["websocket"],
+});
+
 const RaidTarget = () => {
+  const unblockDateFormatted = localStorage.getItem("unblockDateFormattedRaid");
+  const botRaidMessage = localStorage.getItem("bot-raid-message");
+
   const { ref, inView } = useInView({ threshold: 0.1 });
   const { mutate: registerRaidAccess } = useRegisterRaidAccess();
   const { mutate: registerChatMessageInRaid } = useRegisterChatMessageInRaid();
@@ -45,8 +55,11 @@ const RaidTarget = () => {
   const { data, isFetching } = useRaidData();
   const [progress, setProgress] = useState(20);
   const [isCopied, setIsCopied] = useState(false);
-  const [botMessage, _setBotMessage] = useState("Participio do raid diário!");
+
+  const [botMessage, setBotMessage] = useState("");
   const [alertMessage, setAlertMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isBlocked, setIsBlocked] = useState(false);
 
   const openLink = () => {
     registerRaidAccess();
@@ -54,8 +67,15 @@ const RaidTarget = () => {
   };
 
   const generateAiMessage = () => {
-    // limit: 1 per user in a day
+    if (isBlocked || isLoading) return;
+
+    const platform = data?.platform;
+    const shareMessage = data?.shareMessage;
+
+    setIsLoading(true);
+
     registerChatMessageInRaid();
+    socket.emit("raid-message", { platform, shareMessage });
   };
 
   const copyAiMessage = () => {
@@ -86,6 +106,66 @@ const RaidTarget = () => {
 
     updateProgress();
     const interval = setInterval(updateProgress, 60000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("🔗 Connected to WebSocket");
+    });
+
+    socket.on("disconnect", () => {
+      console.log("❌ Disconnected from WebSocket");
+    });
+
+    socket.on("bot-raid-message", (message: BotMessage) => {
+      setBotMessage(message);
+      setIsLoading(false);
+
+      localStorage.setItem("bot-raid-message", message);
+    });
+
+    socket.on(
+      "raid-blocked",
+      ({ unblockDateFormatted, unblockDate }: BlockedMessagePayload) => {
+        setIsLoading(false);
+
+        localStorage.setItem("isBlockedRaid", "true");
+        localStorage.setItem("unblockDateRaid", unblockDate);
+        localStorage.setItem("unblockDateFormattedRaid", unblockDateFormatted);
+        setIsBlocked(true);
+      },
+    );
+
+    return () => {
+      socket.off("bot-raid-message");
+      socket.off("raid-blocked");
+    };
+  }, []);
+
+  useEffect(() => {
+    const checkUnblockTime = () => {
+      const unblockDateStored = localStorage.getItem("unblockDateRaid");
+
+      if (unblockDateStored) {
+        const now = new Date().getTime();
+        const unblockTime = new Date(unblockDateStored).getTime();
+
+        if (now >= unblockTime) {
+          localStorage.removeItem("isBlockedRaid");
+          localStorage.removeItem("unblockDateRaid");
+          localStorage.removeItem("unblockDateFormattedRaid");
+          localStorage.removeItem("bot-raid-message");
+
+          setIsBlocked(false);
+        }
+      }
+    };
+
+    checkUnblockTime();
+
+    const interval = setInterval(checkUnblockTime, 60000);
 
     return () => clearInterval(interval);
   }, []);
@@ -144,12 +224,27 @@ const RaidTarget = () => {
               Gerar mensagem IA
             </Button>
           </SheetTrigger>
-          <SheetContent className="space-between mt-[30px] flex h-full w-[400px] flex-col sm:w-[540px]">
+          <SheetContent
+            className="space-between mt-[30px] flex h-full w-[400px] flex-col sm:w-[540px]"
+            aria-describedby={undefined}
+          >
             <div className="flex flex-col justify-between w-full h-full pb-4">
               <SheetHeader>
                 <SheetTitle className="select-none">Mensagem gerada</SheetTitle>
-                <SheetDescription>{botMessage}</SheetDescription>
+
+                <div className="text-sm text-muted-foreground">
+                  {isBlocked && (
+                    <p className="mb-2 text-xs text-red-500">
+                      Limite atingido! Disponível em {unblockDateFormatted}. ⏳
+                    </p>
+                  )}
+
+                  {botRaidMessage && <span>{botRaidMessage}</span>}
+
+                  {isLoading ? <BotMessageSkeleton /> : botMessage}
+                </div>
               </SheetHeader>
+
               <SheetFooter>
                 <Button className="w-full select-none" onClick={copyAiMessage}>
                   {isCopied ? <Check size={16} /> : <Copy size={16} />}
@@ -198,3 +293,13 @@ const RaidTarget = () => {
 };
 
 export default RaidTarget;
+
+const BotMessageSkeleton = () => {
+  return (
+    <span className="flex flex-col space-y-2">
+      <Skeleton className="w-full h-2" />
+      <Skeleton className="w-1/2 h-2" />
+      <Skeleton className="h-2 w-[10%]" />
+    </span>
+  );
+};
